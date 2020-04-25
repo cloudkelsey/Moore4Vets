6f877133f6f2530b651f96b9a1fe75a35ed4e5eb
Roebuck <milohughes85@gmail.com>
2020-04-25 21:16:29 +0100

    Reverted game speed change

--- common/defines/moore4vets_defines.lua
+++ common/defines/moore4vets_defines.lua
@@ -1,6 +1,6 @@
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 31
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 30
-NDefines.NGame.GAME_SPEED_SECONDS = { 0.5, 0.2, 0.1, 0.05, 0.0 }
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.5, 0.3, 0.2, 0.1, 0.0 }
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 24
 
 NDefines.NDiplomacy.GUARANTEE_COST = 999
